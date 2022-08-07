ec7dfb7cf86e91d610c801c7b4b874d71a2df0fd
Peter Rostin <russin@rcqr.com>
2022-08-07 14:18:02 -0700

    Change to zingneo from felixge

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 
 	_ "net/http/pprof"
 
-	"github.com/felixge/fgprof"
+	"github.com/zingneo/fgprof"
 )
 
 const (
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/felixge/fgprof
+module github.com/zingneo/fgprof
 
 go 1.14
 
